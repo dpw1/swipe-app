908e38f27df8c4381f9c2d70a3738def37857b9f
dpw1 <diego.boarutto.fortes@gmail.com>
2024-03-30 20:31:51 +0000

    UPD tinder swiping feature

--- src/app/SwipeApp.jsx
+++ src/app/SwipeApp.jsx
@@ -3,33 +3,28 @@ import "./SwipeApp.scss";
 import "hammerjs";
 
 export default function SwipeApp() {
-  const [loaded, setLoaded] = useState(false);
-  const [moving, setMoving] = useState(false);
-  const [moveRight, setMoveRight] = useState(null);
+  const [step, setStep] = useState(1);
+  const [like, setLike] = useState(null); // dislike, like, superlike
+
+  const SWIPE_DISTANCE_Y = -72;
+  const SWIPE_DISTANCE_X = 120;
 
   useEffect(() => {
-    // Gets the photo element
     var el = document.querySelector(".photo");
-    // Creates the object
     var hammerTime = new Hammer(el);
-    // Unlocks vertical pan and pinch
     hammerTime.get("pan").set({ direction: Hammer.DIRECTION_ALL });
     hammerTime.get("pinch").set({ enable: true });
 
-    // When user grabs the photo..
     hammerTime.on("pan", function (ev) {
-      // When the photo start moving, the transition become "none" to avoid delay while dragging
       el.classList.add("moving");
-      // If the photo go 80px left/right, the "nope"/"like" stamp appears using css::after
-      el.classList.toggle("nope", ev.deltaX < -80);
-      el.classList.toggle("like", ev.deltaX > 80);
+
+      el.classList.toggle("nope", ev.deltaX < -120);
+      el.classList.toggle("like", ev.deltaX > 120);
       el.classList.toggle(
         "super_like",
         (ev.deltaY < -72) & (Math.abs(ev.deltaX) < 80),
       );
-      // Calculates photo rotation based on offset
       var rotate = ev.deltaX * ev.deltaY * 4e-4;
-      // And applies the movement
       el.style.transform =
         "translate(" +
         ev.deltaX +
@@ -40,16 +35,13 @@ export default function SwipeApp() {
         "deg)";
     });
 
-    // When user releases the photo..
     hammerTime.on("panend", function (ev) {
-      // Gets the positive value of velocity and X-deslocation
       var absVel = Math.abs(ev.velocity);
       var absDelX = Math.abs(ev.deltaX);
-      // Removes the stamps and retrieve the 300ms transition
+
       el.classList.remove("nope", "like", "super_like", "moving");
-      if (absDelX > 80) {
-        // If the photo had a "like"/"dislike" reaction
-        // Photo fades faster if dragged faster, beetwen 400 and 150ms
+
+      if (absDelX > SWIPE_DISTANCE_X) {
         var transitionDuration =
           250 / (absVel + 0.4) > 150
             ? 250 / (absVel + 0.4) > 400
@@ -58,7 +50,6 @@ export default function SwipeApp() {
             : 150;
         el.style.transitionDuration = transitionDuration + "ms";
         var rotate = ev.deltaX * ev.deltaY * 4e-4;
-        // And is thrown farther too
         var mult = absVel > 1.4 ? absVel : 1.4;
         el.style.transform =
           "translate(" +
@@ -68,12 +59,15 @@ export default function SwipeApp() {
           "px) rotate(" +
           rotate * mult +
           "deg)";
-        // Fade out
         el.style.opacity = 0;
-        // And the photo returns
+
+        if (ev.deltaX > 0) {
+          setLike("like");
+        } else if (ev.deltaX < 0) {
+          setLike("dislike");
+        }
         repeat(transitionDuration);
-      } else if (ev.deltaY < -72) {
-        // If the photo had a "super like" reaction, very similar code to the previous one
+      } else if (ev.deltaY < SWIPE_DISTANCE_Y) {
         var transitionDuration =
           250 / (absVel + 0.4) > 150
             ? 250 / (absVel + 0.4) > 400
@@ -84,10 +78,12 @@ export default function SwipeApp() {
         var mult = absVel > 2 ? absVel : 2;
         el.style.transform = "translate( 0px, " + ev.deltaY * mult + "px)";
         el.style.opacity = 0;
+
+        setLike("superlike");
         repeat(transitionDuration);
       } else {
-        // If the photo didn't have a reaction, it goes back to the middle
         el.style.transform = "";
+        setLike(null);
       }
     });
 
@@ -100,8 +96,14 @@ export default function SwipeApp() {
       el.style.transform = "scale(1)";
     });
 
-    // The function that brings back the photo
     function repeat(transitionDuration = 350) {
+      setTimeout(function () {
+        setStep(2);
+      }, transitionDuration);
+      return;
+    }
+
+    function restart(transitionDuration = 350) {
       setTimeout(function () {
         el.style.transform = "";
         setTimeout(function () {
@@ -111,21 +113,15 @@ export default function SwipeApp() {
       }, transitionDuration);
     }
 
-    // The "like" and "dislike" animation
     function buttonEvent(reaction) {
-      // Random transition duration beetwen 300ms and 600ms
       var transitionDuration = Math.random() * 300 + 300;
       el.style.transitionDuration = transitionDuration + "ms";
-      // Random X-deslocation beetwen 100px and 400px
       var x = Math.random() * 300 + 100;
-      // Random Y-deslocation beetwen -200px and 200px
       var y = Math.random() * 400 - 200;
       var rotate = x * y * 4e-4;
       if (reaction == "like") {
-        // If the reaction was a "like", stamps "like"
         el.classList.toggle("like");
       } else if (reaction == "dislike") {
-        // If the reaction was a "dislike", stamps "nope" and moves the photo to the left
         el.classList.toggle("nope");
         x *= -1;
       } else if (reaction == "super_like") {
@@ -139,61 +135,40 @@ export default function SwipeApp() {
       repeat(transitionDuration * 0.8);
     }
 
-    // Creates button actions
+    document
+      .querySelector(`.command--redo`)
+      .addEventListener(`click`, function () {
+        setStep(1);
+        restart(0);
+      });
+
     document
       .querySelector(".fa-close")
       .parentNode.addEventListener("click", function () {
         buttonEvent("dislike");
+        setLike("dislike");
       });
 
     document
       .querySelector(".fa-star")
       .parentNode.addEventListener("click", function () {
         buttonEvent("super_like");
+        setLike("superlike");
       });
 
     document
       .querySelector(".fa-heart")
       .parentNode.addEventListener("click", function () {
         buttonEvent("like");
+        setLike("like");
       });
-
-    // Clock
-    var clockTicking = setInterval(clock, 1000);
-    function clock() {
-      var d = new Date(),
-        displayDate;
-      displayDate = d.toLocaleTimeString();
-      document.querySelector(".clock").innerHTML = displayDate.substring(0, 5);
-    }
   }, []);
 
   return (
-    <div className="SwipeApp">
+    <div className={`SwipeApp SwipeApp--step-${step}`}>
       <>
         <div className="smartphone">
           <div className="screen">
-            <div className="topbar">
-              <div className="topbar-left">
-                <div className="clock">00:00</div>
-                <i className="fa fa-youtube-play" />
-              </div>
-              <div className="topbar-middle">
-                <div className="camera" />
-                <div className="camera-lens" />
-                <div className="inner-lens" />
-              </div>
-              <div className="topbar-right">
-                <i className="fa fa-signal" />
-                73%
-                <i className="fa fa-battery-three-quarters" />
-              </div>
-            </div>
-            <nav className="navbar">
-              <i className="fa-solid fa-circle-user" />
-              <i className="fa-solid fa-fire-flame-curved" />
-              <i className="fa-solid fa-comment-dots" />
-            </nav>
             <div className="person">
               <figure className="photo">
                 <div className="personal">
@@ -201,48 +176,60 @@ export default function SwipeApp() {
                     <h2 className="name">Lorem</h2>
                     <h2 className="age">26</h2>
                   </div>
-                  <div className="data">
-                    <div className="about">
-                      <div className="about-icon">
-                        <i className="fa-solid fa-suitcase" />
-                        <i className="fa-solid fa-graduation-cap" />
-                        <i className="fa-solid fa-location-dot" />
-                      </div>
-                      <div className="about-text">
-                        <p>Designer</p>
-                        <p>Stanford University</p>
-                        <p>4 miles away</p>
-                      </div>
-                    </div>
-                    <div className="info">
-                      <i className="fa-solid fa-circle-info" />
-                    </div>
-                  </div>
                 </div>
               </figure>
+              {step === 2 && (
+                <div className="secondStep">
+                  <p>Why did you {like}?</p>
+                </div>
+              )}
             </div>
             <div className="commands">
-              <div className="command">
-                <i className="fa-solid fa-arrow-rotate-left" />
+              <div
+                className="command command--redo"
+                onClick={() => {
+                  //   setStep(1);
+                  //   restart();
+                }}>
+                <i className="fa fa-arrow-rotate-left">
+                  <svg
+                    viewBox="0 -256 1792 1792"
+                    id="svg3447"
+                    version="1.1"
+                    width="100%"
+                    height="100%">
+                    <g
+                      transform="matrix(1,0,0,-1,113.89831,1262.6441)"
+                      id="g3449">
+                      <path
+                        d="M 1536,640 Q 1536,484 1475,342 1414,200 1311,97 1208,-6 1066,-67 924,-128 768,-128 589,-128 431.5,-52 274,24 165.5,161 57,298 18,473 q -3,14 7,27 9,12 25,12 h 199 q 23,0 30,-23 Q 329,327 464,227.5 599,128 768,128 872,128 966.5,168.5 1061,209 1130,278 q 69,69 109.5,163.5 40.5,94.5 40.5,198.5 0,104 -40.5,198.5 Q 1199,933 1130,1002 1061,1071 966.5,1111.5 872,1152 768,1152 670,1152 580,1116.5 490,1081 420,1015 L 557,877 q 31,-30 14,-69 -17,-40 -59,-40 H 64 Q 38,768 19,787 0,806 0,832 v 448 q 0,42 40,59 39,17 69,-14 l 130,-129 q 107,101 244.5,156.5 137.5,55.5 284.5,55.5 156,0 298,-61 142,-61 245,-164 103,-103 164,-245 61,-142 61,-298 z"
+                        id="path3451"
+                        style={{ fill: "currentColor" }}
+                      />
+                    </g>
+                  </svg>
+                </i>
               </div>
               <div className="command">
-                <i className="fa-solid fa-close" />
+                <i className="fa fa-close">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor">
+                    <path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z" />
+                  </svg>
+                </i>
               </div>
               <div className="command">
-                <i className="fa-solid fa-star" />
+                <i className="fa fa-star" />
               </div>
               <div className="command">
-                <i className="fa-solid fa-heart" />
+                <i className="fa fa-heart"></i>
               </div>
               <div className="command">
-                <i className="fa-solid fa-bolt" />
+                <i className="fa fa-bolt" />
               </div>
             </div>
-            <footer>
-              <i className="fa fa-reorder" />
-              <i className="fa fa-square-o" />
-              <i className="fa fa-chevron-left" />
-            </footer>
           </div>
         </div>
         {/* Font Awesome */}
