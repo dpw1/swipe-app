2a05ccaf797623fdad4e6ce989bd7543464649e6
dpw1 <diego.boarutto.fortes@gmail.com>
2024-06-03 06:58:12 +0100

    [ADD] faq

--- src/app/FAQ.jsx
+++ src/app/FAQ.jsx
@@ -1,8 +1,72 @@
 import React, { useEffect, useState } from "react";
 import "./FAQ.scss";
 
+const faqs = [
+  {
+    question: "Who rates my pictures?",
+    answer:
+      "Your pictures are rated by a diverse community of users from around the globe. You have the option to filter the reviewers based on their gender, age, and location to get feedback that’s most relevant to you.",
+  },
+  {
+    question: "How is toxic feedback dealt with?",
+    answer:
+      "We utilize advanced AI technology to filter out toxic feedback, ensuring that only constructive and respectful comments reach you. Disrespectful behavior is not tolerated, and users who violate our community guidelines will be banned to maintain a positive and supportive environment.",
+  },
+  {
+    question: "Where are my pictures stored?",
+    answer:
+      "Your pictures are securely stored using a third-party service known as Cloudflare. We ensure that your data is handled with the highest standards of security and privacy.",
+  },
+  {
+    question: "Can I delete my pictures after uploading?",
+    answer:
+      "Absolutely. Once you delete your picture, it is permanently removed from our servers and cannot be recovered.",
+  },
+  {
+    question: "Is it free?",
+    answer:
+      'RateMyPicture is free to use! For faster results, you can purchase credits. Additionally, if you\'d like to unlock the "age" and "location" filters, you can purchase the "unlock filters" option. Buying credits will automatically unlock these filters as well. By default, only the gender filter is available for free users.',
+  },
+];
+
+const FAQItem = ({ faq, index }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`faq-item `} data-aos="fade-up">
+      <div className="card shadow-box shadow-hover mb-3">
+        <div
+          className="card-header py-3"
+          onClick={(e) => {
+            toggleCollapse(e);
+          }}>
+          <a
+            href="#"
+            className={`card-title ${isOpen ? "" : "collapsed"} `}
+            data-target
+            data-toggle="collapse">
+            {faq.question}
+          </a>
+        </div>
+        <div
+          className={`collapse card-body ${isOpen ? "show" : ""}`}
+          id={`faq-${index}`}>
+          {faq.answer}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function FAQ() {
   const questions = [];
+
+  const Questions = () => {};
 
   return (
     <section className="faq section faqs-dd bg-light edge top-left">
@@ -14,87 +78,16 @@ export default function FAQ() {
       <div className="container">
         <div className="section-heading text-center">
           <h2 className="heading-line">Frequently Asked Questions</h2>
-          <p className="lead">Want to know more about our services?</p>
+          <p className="lead">
+            Find out more about our platform through these frequently asked
+            questions.
+          </p>
         </div>
         <div className="row">
           <div className="col-lg-8 mx-lg-auto">
-            <div data-aos="fade-up">
-              <div className="card shadow-box shadow-hover mb-3">
-                <div className="card-header py-3">
-                  <a
-                    href="#"
-                    className="card-title collapsed"
-                    data-toggle="collapse"
-                    data-target="#v1-q1">
-                    What does the package include?
-                  </a>
-                </div>
-                <div id="v1-q1" className="collapse card-body">
-                  When you buy Dashcore, you get all you see in the demo but the
-                  images. We include SASS files for styling, complete JS files
-                  with comments, all HTML variations. Besides we include all
-                  mobile PSD mockups.
-                </div>
-              </div>
-            </div>
-            <div data-aos="fade-up">
-              <div className="card shadow-box shadow-hover mb-3">
-                <div className="card-header py-3">
-                  <a
-                    href="#"
-                    className="card-title collapsed"
-                    data-toggle="collapse"
-                    data-target="#v1-q2">
-                    What is the typical response time for a support question?
-                  </a>
-                </div>
-                <div id="v1-q2" className="collapse card-body">
-                  Since you report us a support question we try to make our best
-                  to find out what is going on, depending on the case it could
-                  take more or les time, however a standard time could be
-                  minutes or hours.
-                </div>
-              </div>
-            </div>
-            <div data-aos="fade-up">
-              <div className="card shadow-box shadow-hover mb-3">
-                <div className="card-header py-3">
-                  <a
-                    href="#"
-                    className="card-title collapsed"
-                    data-toggle="collapse"
-                    data-target="#v1-q3">
-                    What do I need to know to customize this template?
-                  </a>
-                </div>
-                <div id="v1-q3" className="collapse card-body">
-                  Our documentation give you all you need to customize your
-                  copy. However you will need some basic web design knowledge
-                  (HTML, Javascript and CSS)
-                </div>
-              </div>
-            </div>
-            <div data-aos="fade-up">
-              <div className="card shadow-box shadow-hover mb-3">
-                <div className="card-header py-3">
-                  <a
-                    href="#"
-                    className="card-title collapsed"
-                    data-toggle="collapse"
-                    data-target="#v1-q4">
-                    Can I suggest a new feature?
-                  </a>
-                </div>
-                <div id="v1-q4" className="collapse card-body">
-                  Definitely <span className="bold">Yes</span>, you can contact
-                  us to let us know your needs. If your suggestion represents
-                  any value to both we can include it as a part of the theme or
-                  you can request a custom build by an extra cost. Please note
-                  it could take some time in order for the feature to be
-                  implemented.
-                </div>
-              </div>
-            </div>
+            {faqs.map((faq, index) => (
+              <FAQItem key={index} faq={faq} index={index} />
+            ))}
           </div>
         </div>
       </div>
