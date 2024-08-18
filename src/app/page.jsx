0071fb938b4ff2c40e8b50f9f678a88094dcf8b8
dpw1 <diego.boarutto.fortes@gmail.com>
2024-08-18 16:00:38 +0100

    ADD fb login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,14 @@ import Footer from "./Footer";
 import EmailSignup from "./EmailSignup";
 import FAQ from "./FAQ";
 
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://kjsscbcikciebrzjgbcv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtqc3NjYmNpa2NpZWJyempnYmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2Mjk0MDAsImV4cCI6MjAzOTIwNTQwMH0.L-8T_MvyqvQcR1dfeTO1Td62SsSadM2qTkZjvkDO0Io",
+);
+
 const lato = Lato({
   weight: ["400", "700", "900"],
   subsets: ["latin"],
@@ -28,6 +36,12 @@ export default function Home() {
     })();
   }, []);
 
+  async function signInWithFacebook() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "facebook",
+    });
+  }
+
   return (
     <main className={lato.className}>
       <Hero></Hero>
@@ -35,6 +49,25 @@ export default function Home() {
       <About></About>
       <FAQ></FAQ>
       <EmailSignup></EmailSignup>
+      <div style={{ marginBottom: "100vh" }}>
+        <p>Login</p>
+
+        <button
+          onClick={async () => {
+            console.log(`click`);
+            const { data, error } = await supabase.auth.signInWithOAuth({
+              provider: "facebook",
+            });
+            await supabase.auth.signInWithOAuth({
+              provider,
+              options: {
+                redirectTo: `https://ratemypicture.app/`,
+              },
+            });
+          }}>
+          Sign in
+        </button>
+      </div>
       <Footer></Footer>
     </main>
   );
